ad3a79405b98e6229ad7aeb0974a42a98fe8132c
JULIÁN WEGMAN <47806018@comunidadort.edu.ar>
2024-06-10 14:28:46 -0300

    funcional EPICO

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express= require('express')
-const cors = require('cors');
+import  express  from 'express';
+import cors from 'cors'
 const app=express();
-
+app.use(express.json())
 app.use(cors());
 
 app.get("/apiAFO", async (req,res)=>{
-    res.json({"oficina":["calle","olaya"]})
+    res.json({"oficina":["calle ","olaya"]})
 })
 
 
